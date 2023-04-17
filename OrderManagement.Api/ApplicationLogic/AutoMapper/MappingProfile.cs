1b3640a43d5d635585d41b8ce42cd5bd2a4b2731
Kapuetze <joni@vocalpatrioten.de>
2023-04-17 09:38:23 +0200

    Fix Automapper

--- OrderManagement.Api/ApplicationLogic/AutoMapper/MappingProfile.cs
+++ OrderManagement.Api/ApplicationLogic/AutoMapper/MappingProfile.cs
@@ -5,9 +5,10 @@ using OrderManagement.Core.Models;
 public class MappingProfile : Profile {
     public MappingProfile() {
 
-    // Mapping for BaseDTO
-    CreateMap<BaseModel, BaseModelDTO>()
-        .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.UniqueId));
+        // Mapping for BaseDTO
+        CreateMap<BaseModel, BaseModelDTO>()
+            .IncludeAllDerived()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.UniqueId));
 
         // Add as many of these lines as you need to map your objects
         CreateMap<User, UserDTO>().ReverseMap();
